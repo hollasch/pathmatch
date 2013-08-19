cdeff6ea18c07c783a4f39139ad54a42d6e9535d
Steve Hollasch <steve@hollasch.net>
2013-08-19 14:21:19 -0700

    Merged wildcomp.* into pathmatcher.*.

--- src/PathMatchLib/pathmatcher.h
+++ src/PathMatchLib/pathmatcher.h
@@ -17,11 +17,48 @@
 
     // Includes
 
-#include "wildcomp.h"
-
 #include <io.h>
 #include <stdlib.h>
 #include <windows.h>
+
+
+    // Function Declarations
+
+bool wildcomp  (const wchar_t *pattern, const wchar_t *string);
+bool wildcompc (const wchar_t *pattern, const wchar_t *string);
+bool pathmatch (const wchar_t *pattern, const wchar_t *path);
+
+
+    // Function Definitions
+
+// Return true if and only if the string begins with "...".
+
+inline bool IsEllipsis (const wchar_t *str)
+{
+    return (str[0] == L'.') && (str[1] == L'.') && (str[2] == L'.');
+}
+
+// Return true if and only if the string begins with a wildcard that matches
+// multiple characters ("*" or "...").
+
+inline bool IsMultiWildStr (const wchar_t * str)
+{
+    return (*str == L'*') || IsEllipsis(str);
+}
+
+// Return true if and only if the string begins with a wildcard.
+
+inline bool IsWildStr (const wchar_t * str)
+{
+    return (*str == L'?') || IsMultiWildStr(str);
+}
+
+// Return true if and only if the character is a forward or backward slash.
+
+inline bool IsSlash (const wchar_t c)
+{
+    return ((c == L'/') || (c == L'\\'));
+}
 
 
 // The callback function signature that PathMatcher uses to report back all matching entries.
