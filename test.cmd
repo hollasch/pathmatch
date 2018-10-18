e140ef1d8fa88a435571ff0464d84a6de0e01c87
Steve Hollasch <steve@hollasch.net>
2018-10-17 21:31:44 -0700

    First working version acceptance tests

--- test.cmd
+++
@@ -1,9 +0,0 @@
-@echo off & setlocal 
-
-set testOut=..\build\tests
-
-cd test
-rmdir /s /q %testOut%
-mkdir >nul 2>&1 %testOut%
-
-call run-tests.cmd
--- test/run-tests.cmd
+++ test/run-tests.cmd
@@ -3,8 +3,40 @@ REM ----------------------------------------------------------------------------
 REM pathmatch Unit Tests
 REM ------------------------------------------------------------------------------------------------
 
+REM ------------------------------------------------------------------------------------------------
+REM // Run Tests
+
+cd /d %~p0
+set testDir=%cd%
+set testOutdir=..\out\tests
+
+rmdir /s /q %testOutDir%
+mkdir >nul 2>&1 %testOutDir%
+
+if /i "%~1" equ "debug" (
+    pushd ..\out\Debug
+        path !cd!;!path!
+    popd
+    echo Testing Debug
+) else (
+    pushd ..\out\Release
+        path !cd!;!path!
+    popd
+    echo Testing Release
+)
+
+if not defined DIFF set DIFF=diff
+
+
 set passCount=0
 set testCount=0
+
+echo.
+for %%f in (test-*.gold) do (
+    call :runTest %%f
+)
+echo.
+
 
 if %passCount% equ %testCount% (
     echo All ^(%testCount%^) tests passed.
@@ -20,3 +52,45 @@ if %failCount% equ 1 (
 )
 
 exit /b 1
+
+
+REM ------------------------------------------------------------------------------------------------
+:runTest
+    set testName=%~n1
+    set testCommand=
+    for /f "delims=" %%c in (%1) do (
+        set testCommand=%%c
+        goto :break
+    )
+    :break
+
+    set testOutput=%testOutDir%\%testName%.out
+    echo %testCommand%|eol \n > %testOutput%
+    echo.|eol \n >>%testOutput%
+    %testCommand% |eol \n >>%testOutput%
+
+    fc >nul 2>&1 %testName%.gold %testOutput%
+    if %ErrorLevel% neq 0 goto :fail
+        set /a passCount = !passCount! + 1
+        echo pass - %testName%
+        goto :endif
+    :fail
+        echo fail - %testName%
+        :query
+        set action=n
+        set /p action="       [n]ext, [d]iff, [a]ccept? (default is next) "
+        if /i "!action!" equ "d" (
+            call %DIFF% %testName%.gold %testOutput%
+            goto :query
+        ) else if /i "!action!" equ "a" (
+            copy >nul /y %testOutput% %testName%.gold
+            set /a passCount = !passCount! + 1
+            goto :endif
+        ) else if /i "!action!" equ "n" (
+            goto :endif
+        )
+        goto :query
+    :endif
+
+    set /a testCount = %testCount% + 1
+    goto :eof
